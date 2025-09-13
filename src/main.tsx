2eca5a8e9ff47c58dd70f97e685f55aca18d4ede
TienThanh17 <huynhthanhtien1711@gmail.com>
2025-09-13 16:33:44 +0700

    add toggle theme mode

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import AuthProvider from "./context/AuthContext";
+import ThemeProvider from "@/context/ThemeContext";
 import { QueryProvider } from "./lib/react-query/QueryProvider";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <QueryProvider>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
+      <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </ThemeProvider>
     </QueryProvider>
   </BrowserRouter>
 );
